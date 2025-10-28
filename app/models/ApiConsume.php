833443a32041ee60b37435f489343a26e87eb923
Maricelahuaman <yucracuroroliber@gmail.com>
2025-10-28 09:23:30 -0500

    url

--- app/models/ApiConsume.php
+++ app/models/ApiConsume.php
@@ -3,7 +3,7 @@
 class ApiConsume {
 
     private $pdo;
-    private $apiBase = "";
+    private $apiBase = "https://muni.serviciosvirtuales.com.pe/api.php";
 
     public function __construct($pdo) {
         $this->pdo = $pdo;
