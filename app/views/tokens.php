78c4236ac9962fbc00886004e4073f10b7e579ab
roliberyucra <yucracuroroliber@gmail.com>
2025-11-10 07:44:35 -0500

    Update tokens.php

--- app/views/tokens.php
+++ app/views/tokens.php
@@ -2,7 +2,7 @@
 <?php $title = "Token Actual"; ?>
 
 <?php
-$url = "https://www.muni.serviciosvirtuales.com.pe/index.php?action=clienteApiRequest";
+$url = "https://www.muni.serviciosvirtuales.com.pe/api.php";
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_POST, true);
